e877c9a7b12c8c94cd1abd19b6f1b3bf05e838e4
Taufeq  Razakh <>
2018-11-06 20:38:25 +0530

    Started my stock option project

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for option-caller
+
+## Unreleased changes
---
+++ src/Types/Tick.hs
@@ -0,0 +1,32 @@
+-- http://hackage.haskell.org/package/aeson-1.4.1.0/docs/Data-Aeson.html
+-- { "name": "Joe", "age": 12 }
+
+-- {-# LANGUAGE DeriveGeneric #-}
+
+-- import GHC.Generics
+
+-- data Person = Person {
+--       name :: Text
+--     , age  :: Int
+--     } deriving (Generic, Show)
+
+
+module Types.Tick where 
+
+import GHC.Generics
+
+-- 2018-11-05 13:35:00 
+-- 1. open "106.9510"
+-- 2. high "107.0100"
+-- 3. low  "106.7000"
+-- 4. close    "106.7248"
+-- 5. volume   "260934"
+
+-- using field labels
+data Tick = Tick {
+    open :: Double
+  , high :: Double
+  , low :: Double
+  , close :: Double
+  , volume :: Int
+    }
