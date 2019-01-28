c51cac21bca46559049b4329a0ce1f061f032b42
Taufeq  Razakh <>
2019-01-28 18:37:14 +0530

    added cases to from String to Option parser

--- src/Types/Option.hs
+++ src/Types/Option.hs
@@ -11,10 +11,19 @@ data Option = Option { symbol :: T.Text
                      } deriving(Read, Show, Eq, Ord)
 
 -- Move typeclass to it's own folder
-class FromTextINput a where
-  fromText :: T.Text -> a
+class FromTextInput a where
+  fromText :: T.Text -> Either T.Text a
 
 instance FromTextInput Option where
   fromText inputText = let inputSet = T.words inputText
                        in case inputSet of
-                            (symbol:xs) ->  
+                            [] -> Left "Insufficient input"
+                            (symbol:toTime) -> case toTime of
+                              [] -> Left "insufficient input params"
+                              (_:[]) -> Left "insufficient input params"
+                              (expiryDay : expiryTime : toStrike) -> case toStrike of
+                                [] -> Left "insufficient input params"
+                                (strike : toPrice) -> case toPrice of
+                                  [] -> Left "insufficient input params"
+                                  (price:rest) -> Left "good job"
+                              
